2cbfeccb5092d4985aac38f2b36a59c84b60a436
Lawrence De Belen <lbelen@kvc.org>
2025-12-29 18:00:41 -0600

    Hero section improvements

--- week1/day1/src/components/Body.tsx
+++ week1/day1/src/components/Body.tsx
@@ -15,31 +15,49 @@ interface BodyProps {
 
 const Body: React.FC<BodyProps> = ({ cardItems }) => {
   return (
-    <div className='flex justify-center items-center flex-col'>
-      {/* Hero Section */}
-      <h1 className='text-5xl font-semibold mt-4 mb-4'>iPhone</h1>
-      <p className='text-xl mb-4'>
-        Say hello to the latest generation of iPhone.
-      </p>
-      <div className='flex justify-center items-center p-4'>
-        <button className='cursor-pointer rounded-4xl bg-blue-500 px-5 py-2.5 text-lg text-white mr-5'>
-          Learn more
-        </button>
-        <button className='cursor-pointer rounded-4xl bg-white-500 px-5 py-2.5 text-lg text-blue-500 outline outline-2 outline-blue-500'>
-          Shop iPhone
-        </button>
-      </div>
-      <img src={heroImg} alt='iPhone' />
+    <>
+      <section
+        className='flex items-center flex-col bg-cover bg-center w-full h-[700px]'
+        style={{ backgroundImage: `url(${heroImg})` }}
+        aria-label='Hero section'
+      >
+        {/* Hero Section */}
+        <div className='p-5 flex items-center flex-col'>
+          <h1 className='text-5xl font-semibold mt-4 mb-4'>iPhone</h1>
+          <p className='text-[28px]'>
+            Say hello to the latest generation of iPhone.
+          </p>
+        </div>
+        <div
+          className='flex justify-center items-center'
+          aria-label='Hero actions'
+        >
+          <a
+            href='#'
+            className='cursor-pointer rounded-4xl bg-blue-500 px-5 py-2.5 text-lg text-white mr-5 inline-block text-center'
+            role='button'
+          >
+            Learn more
+          </a>
+          <a
+            href='#'
+            className='cursor-pointer rounded-4xl bg-white-500 px-5 py-2.5 text-lg text-blue-500 outline outline-2 outline-blue-500 inline-block text-center'
+            role='button'
+          >
+            Shop iPhone
+          </a>
+        </div>
+      </section>
 
       {/* Hero Card */}
-      <HeroCard
+      {/* <HeroCard
         headerText='Give something special.'
         subText="Find what they've been waiting for all year."
         buttonText='Shop gifts'
-      />
+      /> */}
 
       {/* Products Section */}
-      <div className='flex flex-wrap justify-center'>
+      {/* <div className='flex flex-wrap justify-center'>
         {cardItems.map((item, idx) => (
           <div
             key={item.headerText + '-' + idx}
@@ -53,8 +71,8 @@ const Body: React.FC<BodyProps> = ({ cardItems }) => {
             />
           </div>
         ))}
-      </div>
-    </div>
+      </div> */}
+    </>
   );
 };
 
