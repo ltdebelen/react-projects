5be633d3b4e4b6e3f5ca9ac682d0fc4d37d5afe2
Lawrence De Belen <lbelen@kvc.org>
2026-01-06 16:35:07 -0600

    filter - AdminsOnly

--- week1/homework/src/App.tsx
+++ week1/homework/src/App.tsx
@@ -1,19 +1,13 @@
-import "./App.css";
-import { UserCard } from "./components/UserCard";
+import './App.css';
+import { AdminsOnly } from './filtering/AdminsOnly';
 
 function App() {
-  const components = [];
-
-  for (let i = 0; i < 100; i++) {
-    components.push(<UserCard />);
-  }
-
-  const fruits = ["apple", "orange", "banana"];
-
   return (
     <>
-      <div>{[<div>apple</div>, <div>orange</div>, <div>banana</div>]}</div>
-      <div>{components}</div>
+      <div className='min-h-screen bg-white flex flex-col items-center justify-center'>
+        <h1 className='text-black text-3xl font-bold mb-6'>Filter Exercises</h1>
+        <AdminsOnly />
+      </div>
     </>
   );
 }
