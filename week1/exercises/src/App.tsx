758911922bdde78dc22e1cb6183cf9be7cec24fe
Lawrence De Belen <lbelen@kvc.org>
2026-01-06 20:35:04 -0600

    Capitalize Words

--- week1/exercises/src/App.tsx
+++ week1/exercises/src/App.tsx
@@ -12,11 +12,12 @@ import { MultipleConditions } from './filtering/MultipleConditions';
 import { PreFilter } from './filtering/PreFilter';
 import { PriceGreaterThan20 } from './filtering/PriceGreaterThan20';
 import { SearchSimulation } from './filtering/SearchSimulation';
+import { CapitalizeWords } from './mapping';
 
 function App() {
   return (
     <>
-      <div className='min-h-screen bg-white flex flex-col items-center justify-center gap-4'>
+      {/* <div className='min-h-screen bg-white flex flex-col items-center justify-center gap-4'>
         <h1 className='text-black text-3xl font-bold mb-6'>
           Basic JSX Components - Homework
         </h1>
@@ -31,9 +32,9 @@ function App() {
           altText='Company Logo'
         />
         <FooterComponent year={2026} companyName='Tech Corp' />
-      </div>
+      </div> */}
 
-      <div className='min-h-screen bg-white flex flex-col items-center justify-center gap-4'>
+      {/* <div className='min-h-screen bg-white flex flex-col items-center justify-center gap-4'>
         <h1 className='text-black text-3xl font-bold mb-6'>Filter Exercises</h1>
         <AdminsOnly />
         <CaseInsensitive />
@@ -44,6 +45,13 @@ function App() {
         <PreFilter />
         <PriceGreaterThan20 />
         <SearchSimulation searchTerm={'React'} />
+      </div> */}
+
+      <div className='min-h-screen bg-white flex flex-col items-center justify-center gap-4'>
+        <h1 className='text-black text-3xl font-bold mb-6'>
+          Mapping Exercises
+        </h1>
+        <CapitalizeWords />
       </div>
     </>
   );
--- week1/exercises/src/mapping/CapitalizeWords.tsx
+++ week1/exercises/src/mapping/CapitalizeWords.tsx
@@ -1,13 +1,13 @@
-import ProblemCard from "../components/ProblemCard";
+import ProblemCard from '../components/ProblemCard';
 
 export const CapitalizeWords = () => {
-  const animals = ["dog", "cat", "bird"];
+  const animals = ['dog', 'cat', 'bird'];
 
   return (
     <ProblemCard
-      title="Map 03 — Capitalize words"
-      method="map"
-      question="Render each word with the first letter capitalized."
+      title='Map 03 — Capitalize words'
+      method='map'
+      question='Render each word with the first letter capitalized.'
       dataPreview={animals}
     >
       <ul>
